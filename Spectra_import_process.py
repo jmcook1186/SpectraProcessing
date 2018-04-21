13639dd28f021cdb665a742c5253c8e62eb354c2
Unknown <joe.cook@sheffield.ac.uk>
2018-04-21 13:57:00 +0100

    Interpolate over instabilities at 1800 and 2500 nm
    Now automatically interpolates across the instabilities at 1800 and 2500 nm. Uses pandas linear interpolation and then pandas rolling mean to then smooth the interpolated data. NB in future commit rolling mean should be replaced with series.rolling to avoid deprecated function.

--- Spectra_import_process.py
+++ Spectra_import_process.py
@@ -2,17 +2,17 @@
 """
 Created on Wed Aug 16 10:59:17 2017
 
-@author: Joe
+@author: Joseph Cook, University of Sheffield
 """
 # Code to import spectra from ASCII text files output by ASD's ViewSpec
-# software, extract the values and concatenate into a pandas dataframe.
+# software, extract the values and collate into a pandas dataframe.
 # The columns are named according to the original asd filenames minus the
 # extensions. The metadata and header info is discarded but can be obtained
 # by viewing the original text file.
 
 # Prior to running this code, the asd files have been exported as ascii files 
 # using ViewSpec. This saves the individual files as asd.txt. These are then
-# renamed using in a batch using the command line:
+# renamed in a batch using the command line:
 
 # navigate to folder using cd
 
@@ -24,22 +24,18 @@ Created on Wed Aug 16 10:59:17 2017
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
 import os
 import numbers
-import scipy as sci
 
 path = "/media/joe/FDB2-2F9B/end_season_spectra/"
-# on laptop path = 'D:\\Black and Bloom\\Field Data\\JC2017\\Albedo\\'
-#sys.path.append(path)
 
 filelist = os.listdir(path) # pull all files out of the target directory
+
 # create list of values in each txt file. skip 41 rows to discard header info, define comma delimiter, 
 DF = pd.DataFrame()
 
 for file in filelist:   
     DF[file] = pd.read_csv(path+file)
- 
 
 # rename each column with the filename minus the extension
 filenames = []
@@ -60,19 +56,29 @@ DF = DF2.transpose()
 # drop broken spectra
 DF = DF.drop(['11_8_16_disc_cryoconite2'],axis=1)
 DF = DF.drop(['10_8_16_bumps_2s'],axis=1)
-DF['WL'] = np.arange(350,2500,1)
+DF['Wavelength'] = np.arange(350,2500,1)
 
 DF.plot(figsize=(15,15)),plt.ylim(0,1)
 # correct for 1000nm step 
 
 for i in DF.columns:
-    X = np.array(DF['WL'][640:650])
+    X = np.array(DF['Wavelength'][640:650])
     Y = np.array(DF[i][640:650])
     # calculate linear regression coefficients
     corr = DF[i][650] - DF[i][649]
     DF[i][650:-1] = DF[i][650:-1]-corr
-      
+    
+    # interpolate over instabilities at ~1800 nm
+    DF[i][1480:1600] = np.nan 
+    DF[i] = DF[i].interpolate()
+    DF[i][1400:1600] = pd.rolling_mean(DF[i][1400:1600],window=30)
+    DF[i] = DF[i].interpolate()
 
-DF.plot(figsize=(15,15)),plt.ylim(0,1.2)
+    #interpolate over instabilities at 2500 nm
+    DF[i][2050:2150] = np.nan 
+    DF[i] = DF[i].interpolate()
+    DF[i][2050:2150] = pd.rolling_mean(DF[i][2050:2150],window=30)
+    DF[i] = DF[i].interpolate()
 
+DF.plot(x=['Wavelength'],figsize=(15,15)),plt.ylim(0,1.2)
 DF.to_csv('/media/joe/FDB2-2F9B/2016_end_season_HCRF.csv')
